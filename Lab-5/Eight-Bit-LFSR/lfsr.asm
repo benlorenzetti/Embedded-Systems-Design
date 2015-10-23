c77e12046f3e3f91fd9cd8b851faa1b67d351900
Ben <ben.lorenzetti@gmail.com>
2015-10-22 20:17:27 -0400

    report complete minus circuit photo

--- Lab-5/Eight-Bit-LFSR/lfsr.asm
+++ Lab-5/Eight-Bit-LFSR/lfsr.asm
@@ -7,17 +7,17 @@
 	__CONFIG	_CONFIG1, _LVP_OFF & _FCMEN_OFF & _IESO_OFF & _BOR_OFF & _CPD_OFF & _CP_OFF & _MCLRE_OFF & _PWRTE_ON & _WDT_OFF & _INTRC_OSC_NOCLKOUT
 	__CONFIG	_CONFIG2, _WRT_OFF & _BOR21V
 
-;-----------LFSR Bit/Byte Sizes, Tap Locations, and Initial Values------------;
+;-----------LFSR Size, Tap Location, and Initial Value--------------;
 #define LFSR_SIZE 1		; LFSR byte size
-#define LED_MASK	0xFF	; 0x07 for 3 bit LFSR, 0x0F for 4-bit LFSR, 0xFF for 8-bit LFSR
-#define LFSR1_TAP_MASK	0xB8	; 0x06 for 3-bit LFSR, 0x0C for 4-bit LFSR, 0xB8 for 8-bit LFSR
+#define LED_MASK	0xFF	; Mask LEDs if desired bit size < 8
+#define LFSR1_TAP_MASK0	0xB8	; the Feedback Polynomial
+#define LFSR1_TAP_MASK1	0x00	; (next higher byte if LFSR_SIZE > 1)
 #define INITIAL_VALUE	1
 
 ;-----------------------Organize Program Memory---------------------;
 Reset_Vector
 	ORG 0
 	GOTO Initialize
-
 Interupt_Vector
 	ORG 4
 
@@ -76,7 +76,7 @@ Linear_Xor_Function	MACRO	LFS_Register, Reg_Size, Tap_Mask
 	local i = 0
 	while i < LFSR_SIZE
 	 MOVFW	(LFS_Register + i)	; copy the LSFR to accumulator
-	 ANDLW	(Tap_Mask + i)		; AND accum. copy with TAP_MASK
+	 ANDLW	Tap_Mask#v(i)		; AND accum. copy with TAP_MASK
 	 MOVWF	(LOCAL_LFSR + i) 	; move from accum. to local function variable
 	 i += 1
 	endw
@@ -84,7 +84,7 @@ Linear_Xor_Function	MACRO	LFS_Register, Reg_Size, Tap_Mask
 	CLRF	PROP_SIGNAL		; initialize XOR signal to 0
 	MOVLW	(8 * Reg_Size)		; start XOR adding at most significat bit
 	BCF	STATUS, C		; set initial XOR add input to 0
-Xor_Propagation_Loop
+Xor_Propagation_Loop#v(LFS_Register)
 	;----------- rotate 1 bit from local copy of LSFR ----------;
 	RLF_Word	LOCAL_LFSR, LFSR_SIZE
 	;----------- Add Carry Bit to PROP_SIGNAL ------------------;
@@ -92,11 +92,10 @@ Xor_Propagation_Loop
 	ANDLW	1		; keep only the carry bit
 	ADDWF	PROP_SIGNAL, 1	; add carry bit to prop signal
 	DECFSZ	BIT_INDEX, 1	; break loop after all bits propagated
-	GOTO	Xor_Propagation_Loop
+	GOTO	Xor_Propagation_Loop#v(LFS_Register)
 ;-------Return (1s-Digit of PROP_SIGNAL) via Carry Flag ------------;
 	RRF	PROP_SIGNAL, 1
 	ENDM
-
 
 ;-------------------Initialize Data Memory--------------------------;
 Initialize
@@ -117,7 +116,8 @@ Main
 	;-------------------Cycle LSFR -----------------------------;
 	Linear_Xor_Function	LFSR1, LFSR_SIZE, LFSR1_TAP_MASK
 		; return value is in the Carry Flag
-	RLF_Word	LFSR1, LFSR_SIZE	; shift LSFR with feedback from STATUS, C
+	RLF_Word	LFSR1, LFSR_SIZE	
+		; shift LSFR with feedback from STATUS, C
 	;---------------Delay 1 Second-----------------------------;
 	CALL	Pause_1_Second
 	GOTO	Main
