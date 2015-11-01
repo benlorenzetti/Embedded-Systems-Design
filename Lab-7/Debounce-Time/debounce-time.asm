02e279d77d9fa9c5b62f18e6c532ef7b744a8bb7
Ben <ben.lorenzetti@gmail.com>
2015-11-01 01:31:15 -0400

    having trouble using pushbutton switch

--- Lab-7/Debounce-Time/debounce-time.asm
+++ Lab-7/Debounce-Time/debounce-time.asm
@@ -6,115 +6,90 @@
 	__CONFIG	_CONFIG1, _LVP_OFF & _FCMEN_OFF & _IESO_OFF & _BOR_OFF & _CPD_OFF & _CP_OFF & _MCLRE_OFF & _PWRTE_ON & _WDT_OFF & _INTRC_OSC_NOCLKOUT
 	__CONFIG	_CONFIG2, _WRT_OFF & _BOR21V
 
-#define NEUTRAL_POS		0x80
-#define INNER_DELAY_TIME	0x8F
-#define MIDDLE_DELAY_TIME	0x0F
-#define MINIMUM_HALF_PERIOD	0x06
-#define	OSC8_CHANNEL0_NOGO_ADON	B'01000001'
-#define	LEFTJUSTIFY_VSS_VDD	B'00000000'
-#define RESOLUTION_MASK		B'11111100'
+#define INNER_DELAY_TIME	.1
+#define OUTER_DELAY_TIME	.1
 
-;-----------------------Organize Program Memory---------------------;
+;-----------------------Organize Program Memory------------------------------;
 Reset_Vector
 	ORG 0
-	GOTO Initialize
+	GOTO Main
 
 Interupt_Vector
 	ORG .4
 
 ;--------------------Allocate Static Variables----------------------;
 	cblock	0x20
-	adc_result
-	turn_signal
-	delay_time
+	active_state
 	outer_delay_counter
-	middle_delay_counter
 	inner_delay_counter
 	endc
 
-;----------Pause (INNER_DELAY * MIDDLE_DELAY * delay_time)----------;
-Delay_Function
-	MOVF	delay_time, W		; copy delay_time to
-	MOVWF	outer_delay_counter	;   outer_delay_counter
-	MOVLW	INNER_DELAY_TIME	; initialize
-	MOVWF	inner_delay_counter	;   inner_delay_counter
-	MOVLW	MIDDLE_DELAY_TIME	; initialize
-	MOVWF	middle_delay_counter	;   middle_delay_counter
+;-------------------- Pause (INNER_DELAY * OUTER_DELAY)----------------------;
+Pause
+	MOVLW	OUTER_DELAY_TIME	; 
+	MOVWF	outer_delay_counter	; initialize outer_delay_counter
+	MOVLW	INNER_DELAY_TIME	;
+	MOVWF	inner_delay_counter	; initialize inner_delay_counter
 Inner_Loop
 	DECFSZ	inner_delay_counter, f
 	GOTO	Inner_Loop
 	MOVLW	INNER_DELAY_TIME
 	MOVWF	inner_delay_counter
-Middle_Loop
-	DECFSZ	middle_delay_counter, f
-	GOTO	Inner_Loop
-	MOVLW	MIDDLE_DELAY_TIME
-	MOVWF	middle_delay_counter
 Outer_Loop
 	DECFSZ	outer_delay_counter, f
 	GOTO	Inner_Loop
 	RETURN
 
-;-------------------Initialize Data Memory--------------------------;
-Initialize
-	;------------- Initialize I/O ------------------------------;
-	BANKSEL TRISD		; select Register Bank 1
-	CLRF	TRISD		; set all LED pins to output
-	BANKSEL PORTD		; back to Register Bank 0
-	CLRF	PORTD		; set all LED pins to low
-	BANKSEL	TRISA
-	CLRF	TRISA		; clear TRISA
-	BSF	TRISA, RA0	; set port A pin 0 to input
-	;------------- Initialize ADC-------------------------------;
-	BANKSEL	ADCON1
-	MOVLW	LEFTJUSTIFY_VSS_VDD
-	MOVWF	ADCON1		; left justify result,
-		; use VSS and VDD for Vref- and Vref+
-	BANKSEL	ADCON0	
-	MOVLW	OSC8_CHANNEL0_NOGO_ADON
-	MOVWF	ADCON0		; ADC clock rate = Fosc/8,
-		; ADC input channel = 0, ADC on
-	MOVLW	10		
-	MOVWF	delay_time	; initialize delay_time
-	CALL	Delay_Function	; Pause to allow ADC to settle
-
-;---------------Begin Main Program Loop-----------------------------;
+;------------------------------ Main () -------------------------------------;
 Main
-	;------------ Measure Potentiostat Input -------------------;
-	BANKSEL	ADCON0
-	BSF	ADCON0, GO	; start convertion
-	BTFSC	ADCON0, GO	; is converstion done?
-	GOTO	$-1		; go back to BTFSC instruction
-	BANKSEL ADRESH
-	MOVFW	ADRESH		; store ADC result in W
-	BANKSEL	PORTA		; go back to bank 0
-	;------- Calculate Angular Displacement from Neutral -------;
-	MOVLW	RESOLUTION_MASK	; reduce number of steps by
-	ANDWF	ADRESH, 1	;   truncating lower bits in ADRESH
-	MOVLW	NEUTRAL_POS
-	SUBWF	ADRESH, 1	; compute displacement from Neutral
-	; Z = 1 if ADRESH == NEUTRAL_POS; C = 1 if ADRESH >= NEUTRAL_POS
-	;--------------- Perform Conditional Logic -----------------;
-	BTFSC	STATUS, Z	; test zero flag, skip next if clear
-	GOTO	Main		; if (ADRESH == NEUTRAL_POS)
-	BTFSS	STATUS, C	; if (ADRESH < NEUTRAL_POS), invert
-	COMF	ADRESH, F	;   angular displacement
-	MOVLW	1 << RD7	; assume left turn (ADRESH < NEUTRAL)
-	BTFSC	STATUS, C	; if actually (ADRESH > NEUTRAL_POS),
-	MOVLW	1 << RD0	;   then fix it to be right (RD0)
-	;------------------ Blink LEDs -----------------------------;
-	MOVWF	PORTD		; turn on LED
-	MOVLW	MINIMUM_HALF_PERIOD
-	MOVWF	delay_time	; keep LED on for fixed delay time
-	CALL	Delay_Function	; 
-	CLRF	PORTD		; turn off LEDs
-	MOVF	ADRESH, W	; compute appropriate delay time
-	SUBLW	NEUTRAL_POS + MINIMUM_HALF_PERIOD
-	MOVWF	delay_time	; (from angular displacement value)
-	CALL	Delay_Function	; delay
-	;---------- End of Main Function Loop ----------------------;
+	;--------------- Initialize Variables and I/O -----------------------;
+	BANKSEL	PORTD
+	CLRF	PORTD		; port D serves as the bounce counter
+	BANKSEL	TRISD
+	CLRF	TRISD		; output bounce counter to LEDs
+	BSF	TRISA, RA0	; use potentiometer for input
+	BANKSEL	TRISB
+	CLRF	TRISB
+	DECF	TRISB
+;	BSF	TRISB, RB0	; use pushbutton switch for input
+	BANKSEL	PORTB
+;	MOVF	PORTB, W	; copy initial pushbutton level into W
+;	ANDLW	1 << RB0	; keep only the pushbutton pin
+;	XORLW	0xFF		; invert initial level to get active level
+;	MOVWF	active_state	; initialize active_level
+	CLRF	active_state
+
+First_Active_Event
+	MOVF	PORTB, W	; move pushbutton input into W
+
+	MOVWF	PORTD
+	GOTO	First_Active_Event
+
+
+	XORWF	active_state, W	; compare pushbutton level to active level
+	ANDLW	1 << RB0	; keep only the pushbutton bit
+	BTFSS	STATUS, Z	; break if (PORTB ^ active_state == 1)
+	GOTO	First_Active_Event
+
+Bounce_Counting_Loop
+	INCF	PORTD, F	; increment the bounce counter
+	BTFSC	STATUS, C	; check for overflow
+	DECF	PORTD, F	; a hacky overflow fix...
+	COMF	active_state	; next bounce will be the opposite state
+	CALL	Pause		; vary the pause to test bouce damping rate
+
+Bounce_Or_Reset_Events
+	MOVF	PORTB, W	; load pushbutton input into W
+	XORWF	active_state, W	; compare pushbutton to new active level
+	ANDLW	1 << RB0	; keep only pushbutton bit
+	BTFSS	STATUS, Z	; if (PORTB ^ active_state == 1), then a
+	GOTO	Bounce_Counting_Loop	; bounce occurred and needs recorded
+	MOVF	PORTA, W	; move potentiometer input to W
+	ANDLW	1 << RA0	; keep only potentiometer pin
+	BTFSC	STATUS, Z	; if (Vra0 == 0) then restart main program
 	GOTO	Main
-;-------------------------- End of File ----------------------------;
+	GOTO	Bounce_Or_Reset_Events
+;------------------------------ End of File ---------------------------------;
 	END
 
 
